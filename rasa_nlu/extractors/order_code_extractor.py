2c7e5794220ff69e51a9421af5754edf9512c0ef
Nhuan Tran <nhuantran@brodev.com>
2018-12-12 19:42:19 +0700

    Check order code

--- rasa_nlu/extractors/order_code_extractor.py
+++ rasa_nlu/extractors/order_code_extractor.py
@@ -26,32 +26,32 @@ class OrderCodeExtractor(EntityExtractor):
         # message.set("entities",
         #             message.get("entities", []) + extracted,
         #             add_to_output=True)
-        if message.get("intent") == "order_code":
+        if message.get("intent").get("name") == "order_code":
             for entity_mapper in message.get("entities"):
-                if entity_mapper["entity"] != "order_code":
+                if entity_mapper["entity"] == "order_code":
                     return
-        text = message.text
-        order_code = ""
-        if len(text.split()) == 1:
-            order_code = text
-        else:
-            spacy_nlp = kwargs.get("spacy_nlp", None)
-            doc = spacy_nlp(text)
-            for token in doc:
-                if token.pos_ == "NUM":
-                    order_code = token.text
-                    break
-        if order_code != "":
-            start_idx = text.find(order_code)
-            end_idx = start_idx + len(order_code)
-            entity = {
-                "entity": "order_code",
-                "value": order_code,
-                "start": start_idx,
-                "end": end_idx,
-                "confidence": 1.0,
-                "extractor": self.name,
-            }
-            message.set("entities",
-                        message.get("entities", []) + [entity],
-                        add_to_output=True)
+            text = message.text
+            order_code = ""
+            if len(text.split()) == 1:
+                order_code = text
+            else:
+                spacy_nlp = kwargs.get("spacy_nlp", None)
+                doc = spacy_nlp(text)
+                for token in doc:
+                    if token.pos_ == "NUM":
+                        order_code = token.text
+                        break
+            if order_code != "":
+                start_idx = text.find(order_code)
+                end_idx = start_idx + len(order_code)
+                entity = {
+                    "entity": "order_code",
+                    "value": order_code,
+                    "start": start_idx,
+                    "end": end_idx,
+                    "confidence": 1.0,
+                    "extractor": self.name,
+                }
+                message.set("entities",
+                            message.get("entities", []) + [entity],
+                            add_to_output=True)
